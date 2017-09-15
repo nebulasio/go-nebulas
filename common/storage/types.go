a89f61cc02e7ebdb178b33c0c4f0cb94e981760c
Robin Zhong <robin@nebulas.io>
2017-09-15 16:38:01 +0800

    Storage: add storage package in common dir.

---
+++ common/storage/types.go
@@ -0,0 +1,31 @@
+// Copyright (C) 2017 go-nebulas authors
+//
+// This file is part of the go-nebulas library.
+//
+// the go-nebulas library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// the go-nebulas library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the go-nebulas library.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+package storage
+
+// Storage interface of Storage.
+type Storage interface {
+	// Get return the value to the key in Storage.
+	Get(key []byte) ([]byte, error)
+
+	// Put put the key-value entry to Storage.
+	Put(key []byte, value []byte) error
+
+	// Del delete the key entry in Storage.
+	Del(key []byte) error
+}
