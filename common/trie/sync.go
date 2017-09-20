7cabb293cb5bc81344bc5e312910ab00f2bdb135
Roy Shang <s.shang@nebulas.io>
2017-09-21 00:28:00 +0800

    trie:sync.go, update signature

--- common/trie/sync.go
+++ common/trie/sync.go
@@ -18,7 +18,14 @@
 
 package trie
 
-// Sync trie data from other servers to Storage
-func Sync(rootHash []byte) (*Trie, error) {
-	return nil, nil
+// SyncTrie data from other servers
+// Sync whole trie to build snapshot
+func (t *Trie) SyncTrie(rootHash []byte) error {
+	return nil
+}
+
+// SyncPath from rootHash to key node from other servers
+// Useful for verification quickly
+func (t *Trie) SyncPath(rootHash []byte, key []byte) error {
+	return nil
 }
