29f942c203d6ce83694b0d1b7ff22852103aac4f
uynarud <uynarud@gmail.com>
2017-10-13 14:49:39 -0700

    rpc: cleanup fake test logic in api.

--- rpc/api.go
+++ rpc/api.go
@@ -40,28 +40,22 @@ func (s *APIService) GetAccountState(ctx context.Context, req *rpcpb.GetAccountS
 		return nil, status.Errorf(codes.InvalidArgument, "Address is empty.")
 	}
 
-	// TODO: cleanup dummy logic.
-	bal := util.NewUint128FromInt(996)
-	var nonce uint64
-	if s.server != nil {
-		if neb := s.server.Neblet(); neb != nil {
-			reqAddr, _ := byteutils.FromHex(req.Address)
-			addr, err := core.NewAddress(reqAddr)
-			if err != nil {
-				return nil, err
-			}
-			// TODO: handle specific block number.
-			acct := neb.BlockChain().TailBlock().FindAccount(addr)
-			bal = acct.Balance
-			nonce = acct.Nonce
-		}
-	}
+	neb := s.server.Neblet()
 
-	vb, err := bal.ToFixedSizeByteSlice()
+	reqAddr, _ := byteutils.FromHex(req.Address)
+	addr, err := core.NewAddress(reqAddr)
 	if err != nil {
 		return nil, err
 	}
-	return &rpcpb.GetAccountStateResponse{Balance: vb, TransactionCount: nonce}, nil
+
+	// TODO: handle specific block number.
+	acct := neb.BlockChain().TailBlock().FindAccount(addr)
+
+	fsb, err := acct.Balance.ToFixedSizeByteSlice()
+	if err != nil {
+		return nil, err
+	}
+	return &rpcpb.GetAccountStateResponse{Balance: fsb, TransactionCount: acct.Nonce}, nil
 }
 
 // SendTransaction is the RPC API handler.
@@ -69,52 +63,49 @@ func (s *APIService) SendTransaction(ctx context.Context, req *rpcpb.SendTransac
 	if len(req.From) == 0 {
 		return nil, status.Errorf(codes.InvalidArgument, "Sender address is empty.")
 	}
-	// TODO: cleanup dummy logic.
-	if s.server == nil {
-		return &rpcpb.SendTransactionResponse{Hash: "0x07"}, nil
-	}
 
 	// Validate and sign the tx, then submit it to the tx pool.
-	if neb := s.server.Neblet(); neb != nil {
-		from, err := byteutils.FromHex(req.From)
-		if err != nil {
-			return nil, err
-		}
-		fromAddr, err := core.NewAddress(from)
-		if err != nil {
-			return nil, err
-		}
+	neb := s.server.Neblet()
 
-		to, err := byteutils.FromHex(req.To)
-		if err != nil {
-			return nil, err
-		}
-		toAddr, err := core.NewAddress(to)
-		if err != nil {
-			return nil, err
-		}
-
-		value, err := util.NewUint128FromFixedSizeByteSlice(req.Value)
-		if err != nil {
-			return nil, err
-		}
-
-		tx := core.NewTransaction(neb.BlockChain().ChainID(), fromAddr, toAddr, value, req.Nonce /*req.Data */, nil)
-		if err := neb.AccountManager().SignTransaction(fromAddr, tx); err != nil {
-			return nil, err
-		}
-
-		if err := neb.BlockChain().TransactionPool().Push(tx); err != nil {
-			return nil, err
-		}
-
-		// relay tx
-		pbTx, err := tx.ToProto()
-		if err != nil {
-			return nil, err
-		}
-		neb.P2pManager().Relay(net.MessageTypeNewTx, pbTx)
+	from, err := byteutils.FromHex(req.From)
+	if err != nil {
+		return nil, err
 	}
+	fromAddr, err := core.NewAddress(from)
+	if err != nil {
+		return nil, err
+	}
+
+	to, err := byteutils.FromHex(req.To)
+	if err != nil {
+		return nil, err
+	}
+	toAddr, err := core.NewAddress(to)
+	if err != nil {
+		return nil, err
+	}
+
+	value, err := util.NewUint128FromFixedSizeByteSlice(req.Value)
+	if err != nil {
+		return nil, err
+	}
+
+	tx := core.NewTransaction(neb.BlockChain().ChainID(), fromAddr, toAddr, value, req.Nonce /*req.Data */, nil)
+	if err := neb.AccountManager().SignTransaction(fromAddr, tx); err != nil {
+		return nil, err
+	}
+
+	if err := neb.BlockChain().TransactionPool().Push(tx); err != nil {
+		return nil, err
+	}
+
+	// TODO(leon): should api do the relay?
+	// relay tx
+	pbTx, err := tx.ToProto()
+	if err != nil {
+		return nil, err
+	}
+	neb.P2pManager().Relay(net.MessageTypeNewTx, pbTx)
 
 	// TODO: returns the transaction hash if available.
 	return &rpcpb.SendTransactionResponse{}, nil
