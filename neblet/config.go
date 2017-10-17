a0dafdfbbf3b90744841b62537e7c5ff83609cbc
uynarud <uynarud@gmail.com>
2017-10-17 14:42:24 +0800

    rpc: testing client loads config file.

---
+++ neblet/config.go
@@ -0,0 +1,45 @@
+// Copyright (C) 2017 go-nebulas authors
+//
+// This file is part of the go-nebulas library.
+//
+// the go-nebulas library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// the go-nebulas library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with the go-nebulas library.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+package neblet
+
+import (
+	"io/ioutil"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/nebulasio/go-nebulas/neblet/pb"
+	log "github.com/sirupsen/logrus"
+)
+
+func LoadConfig(filename string) *nebletpb.Config {
+	log.Info("Loading Neb config from file ", filename)
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	str := string(b)
+	log.Info("Parsing Neb config text ", str)
+
+	pb := new(nebletpb.Config)
+	if err := proto.UnmarshalText(str, pb); err != nil {
+		log.Fatal(err)
+	}
+	log.Info("Loaded Neb config proto ", pb)
+	return pb
+}
