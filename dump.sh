48a35fceef1e72751adacaf1bf95d13afef4dba2
Robin Zhong <robin@nebulas.io>
2017-11-14 21:05:09 +0800

    travis: add dump.sh to debug build fail issue.

---
+++ dump.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+/usr/bin/ldd /usr/local/lib/libv8engine.so
+ls -alh /usr/lib/x86_64-linux-gnu/libstdc++.*
+strings /usr/lib/x86_64-linux-gnu/libstdc++.so.6 | grep GCC
+fi
