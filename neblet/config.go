73f50b88464c6791c40efab922192b7bd6f7bdef
Leon <liyang@nebulas.io>
2017-11-20 10:40:31 +0800

    config:if config file is not exist, use default.

--- neblet/config.go
+++ neblet/config.go
@@ -20,6 +20,7 @@ package neblet
 
 import (
 	"io/ioutil"
+	"os"
 
 	"github.com/gogo/protobuf/proto"
 	"github.com/nebulasio/go-nebulas/neblet/pb"
@@ -29,6 +30,9 @@ import (
 // LoadConfig loads configuration from the file.
 func LoadConfig(filename string) *nebletpb.Config {
 	//log.Info("Loading Neb config from file ", filename)
+	if !pathExist(filename) {
+		createFile(filename)
+	}
 	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
@@ -43,4 +47,60 @@ func LoadConfig(filename string) *nebletpb.Config {
 	}
 	//log.Info("Loaded Neb config proto ", pb)
 	return pb
+}
+
+func createFile(filename string) {
+	content := `p2p {
+		port: 51413
+		chain_id: 100
+		version: 1
+	  }
+	  rpc {
+		api_port: 51510
+		management_port: 52520
+		api_http_port: 8090
+		management_http_port: 8191
+	  }
+	  pow {
+		coinbase: "8a209cec02cbeab7e2f74ad969d2dfe8dd24416aa65589bf"
+	  }
+	  
+	  storage {
+		location: "seed.db"
+	  }
+	  
+	  account {
+		# keystore.SECP256K1 = 1
+		signature: 1
+	  
+		# keystore.SCRYPT = 1 << 4
+		encrypt: 16
+	  
+		key_dir: "keydir"
+	  
+		test_passphrase: "passphrase"
+	  }
+	  
+	  influxdb {
+		host: "http://localhost:8086"
+		db: "test"
+		username: "dudu"
+		password: "duduforlove"
+	  }
+	  
+	  metrics {
+		enable: false
+	  }`
+
+	if err := ioutil.WriteFile(filename, []byte(content), 0644); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func pathExist(_path string) bool {
+	_, err := os.Stat(_path)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
 }
