dfed816a221b5157a3260ad5ae3645d3a00cb357
fengzi <fengzi.dreamfly@gmail.com>
2018-01-30 23:55:25 +0800

    nebtestkit:update contract.test.js fix transaction not found bug

--- nebtestkit/cases/stress/contract.test.js
+++ nebtestkit/cases/stress/contract.test.js
@@ -7,20 +7,27 @@ var HttpRequest = require("../../node-request");
 var Neb = Wallet.Neb;
 var neb = new Neb();
 //http://34.205.26.12:8685
-neb.setRequest(new HttpRequest("http://127.0.0.1:8685"));//https://testnet.nebulas.io
+
+
+//local
+//neb.setRequest(new HttpRequest("http://127.0.0.1:8685"));//https://testnet.nebulas.io
+//var ChainID = 100;
+//var from = new Wallet.Account("a6e5eb290e1438fce79f5cb8774a72621637c2c9654c8b2525ed1d7e4e73653f");
+
+//test
+neb.setRequest(new HttpRequest("http://34.205.26.12:8685"));
+var ChainID = 1002;
+
+//neb.setRequest(new HttpRequest("http://35.182.48.19:8685"));
+//var ChainID = 1001;
+
+var from = new Wallet.Account("43181d58178263837a9a6b08f06379a348a5b362bfab3631ac78d2ac771c5df3");
 
 var FS = require("fs");
 
-const AddressNumber = 10;
+const AddressNumber = 100;
 const SendTimes = 10;
 var lastnonce = 0;
-
-var ChainID = 100;
-
-// var master = Wallet.Account.NewAccount();
-// var from = new Wallet.Account("43181d58178263837a9a6b08f06379a348a5b362bfab3631ac78d2ac771c5df3");
-var from = new Wallet.Account("a6e5eb290e1438fce79f5cb8774a72621637c2c9654c8b2525ed1d7e4e73653f");
-// 1a263547d167c74cf4b8f9166cfa244de0481c514a45aa2c
 
 // new account  to get address
 var accountArray = new Array();
@@ -33,18 +40,44 @@ for (var i = 0; i < AddressNumber; i++) {
 neb.api.getAccountState(from.getAddressString()).then(function (resp) {
     console.log("master accountState resp:" + JSON.stringify(resp));
     lastnonce = parseInt(resp.nonce);
-    // console.log("lastnonce:", lastnonce);
+    console.log("lastnonce:", lastnonce);
 });
 
 sleep(2000);
-var nonce = lastnonce;
+
+cliamTokens();
 
 var ContractHash ;
 var ContractAddress;
 
-deployContract(nonce);
+var intervalAccount = setInterval(function () {
+	neb.api.getAccountState(from.getAddressString()).then(function (resp) {
+		console.log("master accountState resp:" + JSON.stringify(resp));
+		var nonce = parseInt(resp.nonce);
+		console.log("lastnonce:", lastnonce, "resp_nonce:", nonce);
 
-function deployContract(nonce){
+		if (lastnonce <= nonce){
+			clearInterval(intervalAccount);
+			deployContract();
+		}
+	});
+}, 2000);
+
+function cliamTokens() {
+	var nonce = lastnonce+1;
+    for (var j = 0; j < AddressNumber; j++) {
+        sendTransaction(nonce, accountArray[j]);
+		++nonce;
+        sleep(30);
+    }
+
+	lastnonce = nonce-1;
+}
+
+function deployContract(){
+
+	var nonce = lastnonce
+	console.log("nonce:"+nonce);
     // create contract
     var bank = FS.readFileSync("../nf/nvm/test/bank_vault_contract.js", "utf-8");
     var contract = {
@@ -67,6 +100,7 @@ function deployContract(nonce){
         checkContractDeployed();
     });
 
+	++lastnonce;
 }
 
 function checkContractDeployed(){
@@ -75,38 +109,30 @@ function checkContractDeployed(){
 
     // contract status and get contract_address 
     var interval = setInterval(function () {
-        if (ContractHash){
-            console.log("getTransactionReceipt hash:"+ContractHash);
-            neb.api.getTransactionReceipt(ContractHash).then(function (resp) {
+		console.log("getTransactionReceipt hash:"+ContractHash);
+		neb.api.getTransactionReceipt(ContractHash).then(function (resp) {
 
-                console.log("tx receipt:" + resp.status);
-                
-                if(resp.status === 1) {
-
-                    clearInterval(interval);
-                    cliamTokens();
-                }
-            }).catch(function (err) {
-                retry++;
-                if (retry > 3) {
-                    console.log(JSON.stringify(err.error));
-                    clearInterval(interval);
-                }
-            });
-        }
+			console.log("tx receipt:" + resp.status);
+			
+			if(resp.status && resp.status === 1) {
+				clearInterval(interval);
+				sendMutilContractTransaction(ContractAddress)
+			}
+		}).catch(function (err) {
+			retry++;
+			console.log("error!", JSON.stringify(err.error));
+			if (retry > 10) {
+				console.log(JSON.stringify(err.error));
+				clearInterval(interval);
+			}
+		});
 
     }, 2000);
 }
 
-function cliamTokens() {
-    for (var j = 0; j < AddressNumber; j++) {
-        sendTransaction(nonce, accountArray[j]);
-        sleep(10);
-    }
-}
 
 function sendTransaction(nonce, address) {
-    var transaction = new Wallet.Transaction(ChainID, from, address, neb.nasToBasic(1), ++nonce);
+    var transaction = new Wallet.Transaction(ChainID, from, address, neb.nasToBasic(1), nonce);
     transaction.signTransaction();
     var rawTx = transaction.toProtoString();
     neb.api.sendRawTransaction(rawTx).then(function (resp) {
@@ -115,55 +141,27 @@ function sendTransaction(nonce, address) {
 }
 
 
+
 // get current height
-// var BeginHeight ;
+var BeginHeight ;
 //
-// neb.api.getNebState().then(function (resp) {
-//
-//     BeginHeight = resp.height;
-//     console.log("get NebState resp:" + JSON.stringify(resp));
-// });
-//
-// sleep(5000);
+function sendMutilContractTransaction(address){
 
+	neb.api.getNebState().then(function (resp) {
+		 BeginHeight = resp.height;
+		 console.log("get NebState resp:" + JSON.stringify(resp));
+	});
 
-
-function getTransactionNumberByHeight(){
-
-    neb.api.getNebState().then(function (resp) {
-        console.log("====================") 
-        var EndHeight = resp.height
-        console.log("BeginHeight:"+BeginHeight+ " EndHeight:"+EndHeight)
-        
-        //console.log("send raw transaction resp:" + JSON.stringify(resp));
-
-        var height = BeginHeight
-        for(;height<=EndHeight;height++){
-            neb.api.getBlockByHeight(height, false).then(function (resp) {
-                if(resp.transactions){
-                    //console.log("master accountState resp:" + JSON.stringify(resp));
-                    console.log(resp.height, resp.transactions.length)
-                    return;
-                }else{
-                    console.log(resp.height, 0)
-                }
-            });
-            sleep(10)
-        }
-    });
-}
-
-
-function sendMutilTransaction(address){
-
+	sleep(1000);
     var nonce = lastnonce;
     var t1 = new Date().getTime();
     for (var j = 0; j < AddressNumber; j++) {
+		nonce = 0;
         sendContractTransaction(0, nonce, accountArray[j], address);
-        nonce = nonce + SendTimes;
+        //nonce = nonce + SendTimes;
     }
 
-
+	lastnonce = SendTimes;
     sleep(1000*SendTimes)
     getTransactionNumberByHeight();
 }
@@ -173,11 +171,12 @@ function sendMutilTransaction(address){
 function sendContractTransaction(sendtimes, nonce, from_address, contract_address) {
     if(sendtimes < SendTimes) {
         var call = {
-            "function": "totalSupply",
-            "args":""
+            "function": "save",
+            "args":"[1]"
         }
 
-        var transaction = new Wallet.Transaction(ChainID, from_address, contract_address, "0", ++nonce, "0", "20000000000", call);
+		console.log("send contract nonce:",nonce);
+        var transaction = new Wallet.Transaction(ChainID, from_address, contract_address, "0", ++nonce, "0", "2000", call);
         transaction.signTransaction();
         var rawTx = transaction.toProtoString();
         neb.api.sendRawTransaction(rawTx).then(function (resp) {
@@ -189,4 +188,39 @@ function sendContractTransaction(sendtimes, nonce, from_address, contract_addres
         });
     } 
     
+}
+
+function getTransactionNumberByHeight(){
+
+	var intervalHeight = setInterval(function () {
+		neb.api.getAccountState(accountArray[0].getAddressString()).then(function (resp) {
+			console.log("master accountState resp:" + JSON.stringify(resp));
+			var nonce = parseInt(resp.nonce);
+			console.log("lastnonce:", lastnonce, "resp_nonce:", nonce);
+
+			if (lastnonce <= nonce){
+				clearInterval(intervalHeight)
+				sleep(2000)
+				neb.api.getNebState().then(function (resp) {
+					console.log("====================") 
+					var EndHeight = resp.height
+					console.log("BeginHeight:"+BeginHeight+ " EndHeight:"+EndHeight)
+					
+					var height = BeginHeight
+					for(;height<=EndHeight;height++){
+						neb.api.getBlockByHeight(height, false).then(function (resp) {
+							if(resp.transactions){
+								//console.log("master accountState resp:" + JSON.stringify(resp));
+								console.log(resp.height, resp.transactions.length)
+								return;
+							}else{
+								console.log(resp.height, 0)
+							}
+						});
+						sleep(10)
+					}
+				});
+			}
+		})
+	},1000);
 }
