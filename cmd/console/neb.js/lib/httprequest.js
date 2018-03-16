d3663f38c2a969164cd21ace556b646cf872f494
Larry Wang <larry@nebulas.io>
2018-03-17 00:28:34 +0800

    neb.js: revert request

--- cmd/console/neb.js/lib/httprequest.js
+++ cmd/console/neb.js/lib/httprequest.js
@@ -1,83 +1,90 @@
+
 "use strict";
 
-// must run: npm install promise
-// Node
-var Promise = require('promise/lib/es6-extensions');
 var XMLHttpRequest;
 
 // browser
 if (typeof window !== "undefined" && window.XMLHttpRequest) {
-    XMLHttpRequest = window.XMLHttpRequest;
-
-    // node
+	XMLHttpRequest = window.XMLHttpRequest;
+	// node
 } else {
-    XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-
+	XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	// XMLHttpRequest = require('xhr2');
 }
 
 var HttpRequest = function (host, timeout) {
-    this.host = host || "http://localhost:8685";
-    this.timeout = timeout || 0;
+	this.host = host || "http://localhost:8685";
+	this.timeout = timeout || 0;
 };
 
 HttpRequest.prototype.setHost = function (host) {
-    this.host = host || "http://localhost:8685";
+	this.host = host || "http://localhost:8685";
 };
 
 HttpRequest.prototype._newRequest = function (method, api, async) {
-    var request = new XMLHttpRequest();
-    var m = "GET";
-    if (method.toUpperCase() === "POST") {
-        m = "POST";
-    }
-    var url = this.host + api;
-    request.open(m, url, async);
-    return request;
+	var request = new XMLHttpRequest();
+	var m = "GET";
+	if (method.toUpperCase() === "POST") {
+		m = "POST";
+	}
+	var url = this.host + api;
+	request.open(m, url, async);
+	return request;
 };
 
 HttpRequest.prototype.request = function (method, api, payload) {
-    var request = this._newRequest(method, api, true);
+	var request = this._newRequest(method, api, false);
+	try {
+		if (payload === undefined || payload === "") {
+			request.send();
+		} else {
+			request.send(JSON.stringify(payload));
+		}
+	} catch (error) {
+		throw error;
+	}
 
-    var promise = new Promise(function (resolve, reject) {
+	var result = request.responseText;
+	try {
+		result = JSON.parse(result);
+	} catch (e) {
+		throw e;
+	}
 
-        request.onload = function () {
-            if (request.readyState === 4 && request.timeout !== 1) {
-
-                resolve(request.responseText);
-
-            }else{
-                reject(Error(request.statusText));
-            }
-        };
-
-        request.onerror = function () {
-            reject(Error("Network Error"));
-        };
-
-        request.ontimeout = function () {
-            reject(new Error("connection timeout"));
-        };
-
-        try {
-            if (payload === undefined || payload === "") {
-                request.send();
-            } else {
-                request.send(JSON.stringify(payload));
-            }
-        } catch (error) {
-            reject(error);
-        }
-    });
-
-    return promise;
+	return result;
 };
 
 HttpRequest.prototype.asyncRequest = function (method, api, payload, callback) {
-    this.request(method, api, payload).then(function (resp) {
-        callback(null, resp);
-    }).catch(function (err) {
-        callback(err);
-    });
+	var request = this._newRequest(method, api, true);
+	request.onreadystatechange = function () {
+		if (request.readyState === 4 && request.timeout !== 1) {
+			var result = request.responseText;
+			var error = null;
+
+			try {
+				result = JSON.parse(result);
+			} catch (e) {
+				var message = !!result && !!result.error && !!result.error.message ? result.error.message : "Invalid response: " + JSON.stringify(result);
+				error = new Error(message);
+			}
+
+			callback(error, result);
+		}
+	};
+
+	request.ontimeout = function () {
+		callback(new Error("connection timeout"));
+	};
+
+	try {
+		if (payload === undefined || payload === "") {
+			request.send();
+		} else {
+			request.send(JSON.stringify(payload));
+		}
+	} catch (error) {
+		callback(error);
+	}
 };
 
 module.exports = HttpRequest;
