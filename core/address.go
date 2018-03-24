53a083614ad9bec740cd281374d10a2ccf7d97ac
Yogi Xun <xun@nebulas.io>
2018-03-25 01:29:24 +0800

    address: add comments

--- core/address.go
+++ core/address.go
@@ -66,31 +66,49 @@ const (
 )
 
 /*
-Address Similar to Bitcoin and Ethereum, Nebulas also adopts elliptic curve algorithm as its basic encryption algorithm for Nebulas accounts. A user’s private key is a randomly generated 256-bit binary number, based on which a 64-byte public key can be generated via elliptic curve multiplication. Bitcoin and Ethereum addresses are computed by public key via the deterministic Hash algorithm, and the difference between them lies in: Bitcoin address has the checksum design aiming to prevent a user from sending Bitcoins to a wrong user account accidentally due to entry of several incorrect characters; while Ethereum doesn’t have such checksum design.
 
-We believe that checksum design is reasonable from the perspective of users, so Nebulas address also includes checksum, for which the specific calculation method is provided as follows:
+[Account Address]
+Similar to Bitcoin and Ethereum, Nebulas also adopts elliptic curve algorithm as its basic encryption algorithm for Nebulas accounts.
+The address is derived from **public key**, which is in turn derived from the **private key** that encrypted with user's **passphrase**.
+Also we have the checksum design aiming to prevent a user from sending _Nas_ to a wrong user account accidentally due to entry of several incorrect characters.
 
-  Data = ripemd160(sha3_256(Public Key))
-  Type = 0x57(Account Address) or 0x58(Contract Address)
-  CheckSum = sha3_256(Type + Data)[0:4]
-  Address = base58(0x19 + Type + Data + CheckSum)
+The specific calculation formula is as follows:
 
-The ripemd160 digest of SHA3-256 digest of a public key serve as the major component of an address, for which another SHA3-256 digest should be conducted and the first 4 bytes should be used as a checksum, which is equivalent to the practice of adding a 4-byte checksum to the end of an Ethereum address. For example:
+	Content = ripemd160( sha3_256( Public Key ) )
+	CheckSum = sha3_256( 0x19 + 0x57 + Content )[0:4]
+	Address = base58( 0x19 + 0x57 + Content + CheckSum )
 
-The standard address of Alice’s Ethereum wallet is 0xdf4d22611412132d3e9bd322f82e2940674ec1bc;
-The final address of Nebulas Wallet should be:  n1M2mcK3mcwGNQS7Kt7wmKadJn97paakkZ9
+ 	0x57 is a one-byte "type code" for account address, 0x19 is a one-byte fixed "padding"
 
-In addition to standard address with 50 characters, we also support extended address in order to ensure the security of transfers conducted by users. The traditional bank transfer design is used for reference: In the process of a bank transfer, bank card number of the remittee should be verified, in addition to which the remitter must enter the name of the remittee. The transfer can be correctly processed only when the bank card number and the name match each other. The generating algorithm for extended address is described as follows:
+The ripemd160 digest of SHA3-256 digest of a public key serve as the major component of an address,
+for which another SHA3-256 digest should be conducted and the first 4 bytes should be used as a checksum. For example:
+The final address of Nebulas Wallet should be:  n1TV3sU6jyzR4rJ1D7jCAmtVGSntJagXZHC
 
-  Data = sha3_256(Public Key)[-20:]
-  CheckSum = sha3_256(Data)[0:4]
-  Address = "0x" + Hex(Data + CheckSum)
+[Smart Contract Address]
+Calculating contract address differs slightly from account, passphrase of contract sender is not required but address & nonce.
+For more information, plz check (https://github.com/nebulasio/wiki/blob/master/tutorials/%5BEnglish%5D%20Nebulas%20101%20-%2003%20Smart%20Contracts%20JavaScript.md) and [rpc.sendTransaction](https://github.com/nebulasio/wiki/blob/master/rpc.md#sendtransaction).
+Calculation formula is as follows:
+
+	Content = ripemd160( sha3_256( tx.from, tx.nonce ) )
+	CheckSum = sha3_256( 0x19 + 0x58 + Content )[0:4]
+	Address = base58( 0x19 + 0x58 + Content + CheckSum )
+
+	0x58 is a one-byte "type code" for smart contract address, 0x19 is a one-byte fixed "padding"
+
+
+[TODO]
+In addition to standard address with 50 characters, we also support extended address in order to ensure the security of transfers conducted by users.
+The traditional bank transfer design is used for reference:
+In the process of a bank transfer, bank card number of the remittee should be verified, in addition to which the remitter must enter the name of the remittee.
+The transfer can be correctly processed only when the bank card number and the name match each other.
+The generating algorithm for extended address is described as follows:
 
   ExtData = Utf8Bytes({Nickname or any string})
   ExtHash = sha3_256(Data + ExtData)[0:2]
-  ExtAddress = Address + Hex(ExtHash)
+  ExtAddress = Account Address + Hex(ExtHash)
 
-An extended address is generated through addition of 2-byte extended verification to the end of a standard address and contains a total of 54 characters. Addition of extended information allows the addition of another element verification to the Nebulas Wallet APP. For example:
+An extended address is generated through addition of 2-byte extended verification to the end of a standard address and contains a total of 54 characters.
+Addition of extended information allows the addition of another element verification to the Nebulas Wallet APP. For example:
 
 	The standard address of Alice’s wallet is  0xdf4d22611412132d3e9bd322f82e2940674ec1bc03b20e40, and the extended address after addition of the nickname "alice" should be 0xdf4d22611412132d3e9bd322f82e2940674ec1bc03b20e40e345.
 	Alice tells Bob the extended address 0xdf4d22611412132d3e9bd322f82e2940674ec1bc03b20e40e345 and her nickname alice.
