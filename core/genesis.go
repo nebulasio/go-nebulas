6c48907cf7fb3a6bcea10a73483729f0d68f2e5e
Roy Shang <roy@nebulas.io>
2018-03-29 23:57:51 +0800

    core: genesis.go. confirm the declaration.

--- core/genesis.go
+++ core/genesis.go
@@ -130,11 +130,20 @@ func NewGenesisBlock(conf *corepb.Genesis, chain *BlockChain) (*Block, error) {
 
 	// genesis transaction
 	declaration := fmt.Sprintf(
-		"%s\n%s\n%s\n%s",
+		"%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s",
+		"Nebulas Genesis Block Manifesto",
 		"Yes! We Believe",
-		"我们认为，区块链是奠基新世界的颠覆式创新，其本质是去中心化的数据确权，确权的数据承载于代币之上，对于在“链上”数据的交互具有不可或缺的作用。",
-		"我们同时看到，真正的区块链社区秉持着开放、共享、透明的精神，逐步建立人类历史上前所未有的大规模协作关系。一个公正并有效的价值发现、激励以及持续进化机制所构成的正反馈生态体系，是这场大规模协作关系蓬勃发展的重要推动力，也是星云对于区块链的伟大使命。",
-		"我们始终坚信，区块链技术会帮助人们抵达更为自由、平等、美好的生活。区块链作为一个全新的生命体和经济体，意味着新的思想和技术，也是新的挑战、机遇和希望。面对未来巨大可能性的感召，不要问区块链能为你做什么，要问你能为区块链做什么。Go！Nebulas",
+		"We believe blockchains are a foundational innovation of the new world. At its essence, this innovation is about the decentralization of data. People will be empowered to claim ownership of their data through tokens, which will enable data to be valued and exchanged by everyone on the blockchain. ",
+		"The blockchain community embodies the values of openness, collaboration, and transparency. An ecosystem created by blockchain believers is a voluntary association anchored by aligned incentives. We believe blockchain represents the social contract of the future, and that this will lead to a civilization where cooperation, inclusion, and the interests of society converge. ",
+		"We believe blockchain will bring more freedom, equality and purpose to the world. We believe blockchain is a digital organism and economic system. Revolutionary ideas and technology to fulfill its destiny. This means challenges, opportunities, and also hope. Don't ask what blockchain can do for you. Ask what you can do for blockchain.",
+		"This is the genesis of Nebulas. ",
+
+		"星云创世区块宣言",
+		"Yes! We Believe",
+		"我们认为，区块链是奠基新世界的颠覆式创新，其本质是去中心化的数据确权。确权的数据承载于通证之上，对于“链上”数据的交互具有不可或缺的作用。",
+		"我们同时看到，真正的区块链社区秉持着开放、共享、透明的精神，逐步建立人类历史上前所未有的大规模协作关系。一个公正并有效的价值发现、激励以及持续进化机制所构成的生态系统，是这场大规模协作关系蓬勃发展的原生推动力，也是星云对于区块链的伟大使命。",
+		"我们始终坚信，区块链技术会帮助人们抵达更为自由、平等、美好的生活。区块链作为一个全新的生命体和经济体，意味着新的思想和技术，也意味着新的挑战、机遇和希望。面对未来巨大可能性的感召，不要问区块链能为你做什么，要问你能为区块链做什么。",
+		"星云正是为此而生。",
 	)
 	declarationTx, err := NewTransaction(
 		chain.ChainID(),
