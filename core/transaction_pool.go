d6a308e64495d66cfe688496d0cb1ae05612e62d
fengzi <fengzi@nebulas.io>
2018-05-07 12:18:32 +0800

    core:transaction_pool.go delete key of bucketlastupdate when bucket is empty

--- core/transaction_pool.go
+++ core/transaction_pool.go
@@ -361,6 +361,7 @@ func (pool *TransactionPool) popTx(tx *Transaction) {
 		pool.candidates.Push(candidate)
 	} else {
 		delete(pool.buckets, tx.from.address.Hex())
+		delete(pool.bucketsLastUpdate, tx.from.address.Hex())
 	}
 }
 
@@ -385,6 +386,7 @@ func (pool *TransactionPool) dropTx() {
 			if longestLen == 1 {
 				pool.candidates.Del(drop)
 				delete(pool.buckets, drop.from.address.Hex())
+				delete(pool.bucketsLastUpdate, drop.from.address.Hex())
 			}
 		}
 	}
@@ -462,19 +464,22 @@ func (pool *TransactionPool) Del(tx *Transaction) {
 				left = bucket.Left().(*Transaction)
 			} else {
 				delete(pool.buckets, left.from.address.Hex())
+				delete(pool.bucketsLastUpdate, left.from.address.Hex())
 				break
 			}
 		}
-		newCandidate := bucket.Left()
-		// replace candidate
-		if oldCandidate != newCandidate {
-			pool.candidates.Del(oldCandidate)
-			if newCandidate != nil {
-				pool.candidates.Push(newCandidate)
+		if bucket.Len() > 0 {
+			newCandidate := bucket.Left()
+			// replace candidate
+			if oldCandidate != newCandidate {
+				pool.candidates.Del(oldCandidate)
+				if newCandidate != nil {
+					pool.candidates.Push(newCandidate)
+				}
 			}
+			//update bucket update time when txs are put on chain
+			pool.bucketsLastUpdate[tx.from.address.Hex()] = time.Now()
 		}
-		//update bucket update time when txs are put on chain
-		pool.bucketsLastUpdate[tx.from.address.Hex()] = time.Now()
 	} else {
 		//remove key of bucketsLastUpdate when bucket is empty
 		delete(pool.bucketsLastUpdate, tx.from.address.Hex())
