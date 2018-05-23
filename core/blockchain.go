c78a589827264cdcebeed065cc88bf6e2f5bb222
Yang Shun <silent@nebulas.io>
2018-05-23 04:45:04 -0400

    No downloading parent when syncing

--- core/blockchain.go
+++ core/blockchain.go
@@ -584,6 +584,10 @@ func (bc *BlockChain) StartActiveSync() bool {
 	return false
 }
 
+func (bc *BlockChain) IsActiveSyncing() bool {
+	return bc.syncService.IsActiveSyncing()
+}
+
 // ConsensusHandler return consensus handler.
 func (bc *BlockChain) ConsensusHandler() Consensus {
 	return bc.consensusHandler
