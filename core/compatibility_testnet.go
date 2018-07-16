2d1a1eb1c878cb0ca54b3643b6ca84b5a4cb6f1d
dreamflyfengzi <fengzi.dreamfly@gmail.com>
2018-07-16 15:59:46 +0800

    core:compatibility_testnet.go update testnet inner height 600200

--- core/compatibility_testnet.go
+++ core/compatibility_testnet.go
@@ -47,14 +47,14 @@ func NewCompatibilityTestNet() Compatibility {
 		v8JSLibVersionHeightMap: &V8JSLibVersionHeightMap{
 			Data: map[string]uint64{
 				"1.0.5": 424400, // v8JSLibVersionControlHeight
-				"1.1.0": 582800,
+				"1.1.0": 600200,
 			},
 			DescKeys: []string{"1.1.0", "1.0.5"},
 		},
 
-		nvmGasLimitWithoutTimeoutHeight:                   582800,
-		wsResetRecordDependencyHeight2:                    582800,
-		transferFromContractFailureEventRecordableHeight2: 582800,
+		nvmGasLimitWithoutTimeoutHeight:                   600200,
+		wsResetRecordDependencyHeight2:                    600200,
+		transferFromContractFailureEventRecordableHeight2: 600200,
 	}
 }
 
