8aa529a964c580c5be485bceb2ce3cf3bff5176f
mrwangyu2 <wangyuxxx@163.com>
2018-09-14 21:34:32 +0800

    Fixed code format.

--- nbre/test/common/gtest_util.cpp
+++ nbre/test/common/gtest_util.cpp
@@ -27,27 +27,49 @@ TEST(test_common_util_byte, from_uint64) {
       << "uint64 max failed";
 }
 
-TEST(test_common_util_byte, fix_bytes_default) {
+TEST(test_common_util_byte, test_default) {
   neb::util::fix_bytes<> fb;
 
-  std::string base58 = fb.to_base58();
+  std::string base58 = fb.to_base58(); 
 
   EXPECT_EQ(base58, "0");
 }
 
-TEST(test_common_util_byte, fix_bytes_Encode) {
+TEST(test_common_util_byte, test_encode) {
   neb::util::fix_bytes<> source = neb::util::fix_bytes<>::from_base58("Hello, world");
   neb::util::fix_bytes<> want({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
 
   EXPECT_EQ(source, want);
 }
 
-TEST(test_common_util_byte, fix_bytes_Decode) {
+TEST(test_common_util_byte, test_decode) {
   neb::util::fix_bytes<> fb({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
 
-  std::string source = fb.to_base58();
+  std::string source = fb.to_base58(); 
 
   EXPECT_EQ(source, "Hello, world");
 }
+
+typedef struct test_source_want {
+  std::string source;
+  neb::util::fix_bytes<> fb;
+} test_source_want_t;
+
+// TEST(test_common_util_byte, test_hex) {
+//
+// test_source_want_t[3] tsw = {
+ // {"a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a",
+   // neb::util::fix_bytes<> fb({167, 255, 198, 248, 191, 30, 215, 102, 81, 193, 71, 86, 160, 97, 214, 98, 245, 128, 255, 77, 228, 59, 73, 250, 130, 216, 10, 75, 128, 248, 67, 74}),
+ // {"3550aba97492de38af3066f0157fc532db6791b37d53262ce7688dcc5d461856",
+   // neb::util::fix_bytes<> fb({53, 80, 171, 169, 116, 146, 222, 56, 175, 48, 102, 240, 21, 127, 197, 50, 219, 103, 145, 179, 125, 83, 38, 44, 231, 104, 141, 204, 93, 70, 24, 86}),
+ // {"blank string", neb::util::fix_bytes<> fb({})}
+//
+//
+//
+// }
+//
+
+}
+
 
 
