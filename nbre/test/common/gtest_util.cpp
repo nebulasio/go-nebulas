70e0d3f125b49ab80c69b63f04ca1a43c863830f
Xuepeng Fan <athrunarthur@gmail.com>
2018-09-14 16:18:15 +0800

    update byte test

--- nbre/test/common/gtest_util.cpp
+++ nbre/test/common/gtest_util.cpp
@@ -14,13 +14,15 @@ TEST(test_common_util, simple) {
 TEST(test_common_util_byte, from_uint64) {
   uint64_t v1 = 0;
   neb::util::bytes b1 = neb::util::number_to_byte<neb::util::bytes>(v1);
-  EXPECT_TRUE(b1 == neb::util::bytes({0, 0, 0, 0, 0, 0, 0, 0}));
+  EXPECT_TRUE(b1 == neb::util::bytes({0, 0, 0, 0, 0, 0, 0, 0})) << " 0 failed";
 
   uint64_t v2 = 1024;
   neb::util::bytes b2 = neb::util::number_to_byte<neb::util::bytes>(v2);
-  EXPECT_TRUE(b2 == neb::util::bytes({0, 0, 0, 0, 0, 0, 4, 0}));
+  EXPECT_TRUE(b2 == neb::util::bytes({0, 0, 0, 0, 0, 0, 4, 0}))
+      << " 1024 failed";
 
   uint64_t v3 = 18446744073709551615u;
   neb::util::bytes b3 = neb::util::number_to_byte<neb::util::bytes>(v3);
-  EXPECT_TRUE(b2 == neb::util::bytes({255, 255, 255, 255, 255, 255, 255, 255}));
+  EXPECT_TRUE(b3 == neb::util::bytes({255, 255, 255, 255, 255, 255, 255, 255}))
+      << "uint64 max failed";
 }
