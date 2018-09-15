50774db6cb9882ceb2d846a37978b5ba414acfca
mrwangyu2 <wangyuxxx@163.com>
2018-09-15 17:22:46 +0800

    Added some test case.

--- nbre/test/common/gtest_util.cpp
+++ nbre/test/common/gtest_util.cpp
@@ -1,11 +1,6 @@
 #include "common/util/byte.h"
 #include <gtest/gtest.h>
 
-typedef struct test_result_want {
-  std::string want;
-  neb::util::fix_bytes<> result;
-} test_result_want_t;
-
 TEST(test_common_util, simple) {
   int32_t v = 123;
   neb::byte_t buf[4];
@@ -32,6 +27,26 @@ TEST(test_common_util_byte, from_uint64) {
       << "uint64 max failed";
 }
 
+//TEST(test_common_util_byte, positive_to_base58) {
+  // neb::util::bytes bytes({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+  // std::string result = bytes.to_base58();
+  // std::string want("Hello, world");
+  // EXPECT_EQ(result, want);
+//}
+
+//TEST(test_common_util_byte, positive_to_hex) {
+  // neb::util::bytes bytes({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+  // std::string result = bytes.to_base58();
+  // std::string want("Hello, world");
+  // EXPECT_EQ(result, want);
+//}
+
+// TEST(test_common_util_byte, positive_from_base58) {
+  // neb::util::bytes want({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+  // neb::util::bytes result = neb::util::bytes::from_base58("0");
+  // EXPECT_EQ(result, want);
+// }
+
 TEST(test_common_util_byte, test_default) {
   neb::util::fix_bytes<> fb;
 
@@ -40,47 +55,80 @@ TEST(test_common_util_byte, test_default) {
   EXPECT_EQ(base58, "11111111111111111111111111111111");
 }
 
-TEST(test_common_util_byte, test_encode) {
-  neb::util::bytes result = neb::util::string_to_byte("Hello, world");
-  neb::util::bytes want(
-      {72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+TEST(test_common_util_byte, positive_fix_bytes_to_base58) {
+  neb::util::fix_bytes<6> fb({32, 119, 111, 114, 108, 100});
+
+  std::string result = fb.to_base58();
+  std::string want = "HAi6xaJX";
 
   EXPECT_EQ(result, want);
 }
 
-TEST(test_common_util_byte, test_decode) {
-  neb::util::bytes fb(
-      {72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+TEST(test_common_util_byte, positive_fix_bytes_to_base58_2) {
+  neb::util::fix_bytes<6> fb({ 0x47, 0x4b, 0xc9 ,0x32});
 
-  std::string result = neb::util::byte_to_string(fb);
+  std::string result = fb.to_base58();
+  std::string want = "HAi6xaJX";
 
-  EXPECT_EQ(result, "Hello, world");
+  EXPECT_EQ(result, want);
 }
 
-TEST(test_common_util_byte, test_hex) {
-  neb::util::fix_bytes<> fb0({167, 255, 198, 248, 191, 30,  215, 102,
-                              81,  193, 71,  86,  160, 97,  214, 98,
-                              245, 128, 255, 77,  228, 59,  73,  250,
-                              130, 216, 10,  75,  128, 248, 67,  74});
-  neb::util::fix_bytes<> fb1({53,  80,  171, 169, 116, 146, 222, 56,
-                              175, 48,  102, 240, 21,  127, 197, 50,
-                              219, 103, 145, 179, 125, 83,  38,  44,
-                              231, 104, 141, 204, 93,  70,  24,  86});
-  neb::util::fix_bytes<> fb2({});
+TEST(test_common_util_byte, positive_fix_bytes_to_hex) {
+  neb::util::fix_bytes<6> fb({132, 11, 111, 104, 18, 100});
 
-  test_result_want_t tsw[3] = {
-      test_result_want(
-          {"a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a",
-           fb0}),
-      test_result_want(
-          {"3550aba97492de38af3066f0157fc532db6791b37d53262ce7688dcc5d461856",
-           fb1}),
-      test_result_want({"blank string", fb2})};
+  std::string result = fb.to_hex();
+  std::string want = "0x7fffb0b23c48";
 
-  for (int i = 0; i < 3; i++) {
-    std::string result_hex = tsw[i].result.to_hex();
-    EXPECT_EQ(result_hex, tsw[i].want);
-  }
+  EXPECT_EQ(result, want);
+}
+
+TEST(test_common_util_byte, positive_fix_bytes_size) {
+  neb::util::fix_bytes<12> fb({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+
+  int result = fb.size();
+  int want = 12;
+
+  EXPECT_EQ(result, want);
+}
+
+TEST(test_common_util_byte, positive_fix_bytes_value) {
+  neb::util::fix_bytes<12> fb({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+
+  neb::byte_t *result = fb.value();
+  std::cout <<  printf("----------ok");
+  neb::byte_t want[4] = {1, 2, 3, 4};
+
+  EXPECT_EQ(result, want);
+}
+
+TEST(test_common_util_byte, positive_fix_bytes_from_base58) {
+  neb::util::fix_bytes<12> result = neb::util::fix_bytes<12>::from_base58("2NEp7TZsLFA2wMeK");
+  neb::util::fix_bytes<12> want({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+
+  EXPECT_EQ(result, want);
+}
+
+TEST(test_common_util_byte, positive_fix_bytes_from_hex) {
+  neb::util::fix_bytes<12> result = neb::util::fix_bytes<12>::from_hex("10AB");
+  neb::util::fix_bytes<12> want({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+
+  EXPECT_EQ(result, want);
+}
+
+TEST(test_common_util_byte, negative_fix_bytes_to_hex) {
+  neb::util::fix_bytes<12> fb({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+
+  std::string result = fb.to_hex();
+  std::string want = "Hello, world";
+
+  EXPECT_EQ(result, want);
+}
+
+TEST(test_common_util_byte, negative_fix_bytes_from_hex) {
+  neb::util::fix_bytes<12> result = neb::util::fix_bytes<12>::from_hex("Hello, world");
+  neb::util::fix_bytes<12> want({72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100});
+
+  EXPECT_EQ(result, want);
 }
 
 
