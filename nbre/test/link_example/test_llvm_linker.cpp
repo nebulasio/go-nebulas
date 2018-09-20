624b8388ab96859a0f7b76484dba7ed8b79cfa0f
Xuepeng Fan <athrunarthur@gmail.com>
2018-09-20 22:05:29 +0800

    add ir.proto

--- nbre/test/link_example/test_llvm_linker.cpp
+++ nbre/test/link_example/test_llvm_linker.cpp
@@ -1,134 +1,105 @@
-#include "llvm/ADT/STLExtras.h"
-#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "OrcLazyJIT.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/ADT/Triple.h"
+#include "llvm/Bitcode/BitcodeReader.h"
+#include "llvm/CodeGen/CommandFlags.def"
+#include "llvm/CodeGen/LinkAllCodegenComponents.h"
 #include "llvm/ExecutionEngine/GenericValue.h"
 #include "llvm/ExecutionEngine/Interpreter.h"
-#include "llvm/IR/Argument.h"
-#include "llvm/IR/BasicBlock.h"
-#include "llvm/IR/Constants.h"
-#include "llvm/IR/DerivedTypes.h"
-#include "llvm/IR/Function.h"
+#include "llvm/ExecutionEngine/JITEventListener.h"
+#include "llvm/ExecutionEngine/MCJIT.h"
+#include "llvm/ExecutionEngine/ObjectCache.h"
+#include "llvm/ExecutionEngine/Orc/OrcRemoteTargetClient.h"
+#include "llvm/ExecutionEngine/OrcMCJITReplacement.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
 #include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/Instructions.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/IR/Type.h"
-#include "llvm/Support/Casting.h"
+#include "llvm/IR/TypeBuilder.h"
+#include "llvm/IRReader/IRReader.h"
+#include "llvm/Object/Archive.h"
+#include "llvm/Object/ObjectFile.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Debug.h"
+#include "llvm/Support/DynamicLibrary.h"
+#include "llvm/Support/Format.h"
 #include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/MathExtras.h"
+#include "llvm/Support/Memory.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/PluginLoader.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/Process.h"
+#include "llvm/Support/Program.h"
+#include "llvm/Support/Signals.h"
+#include "llvm/Support/SourceMgr.h"
 #include "llvm/Support/TargetSelect.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Support/SourceMgr.h"
-#include "llvm/IRReader/IRReader.h"
-#include "llvm/Linker/Linker.h"
+#include "llvm/Transforms/Instrumentation.h"
+#include <cerrno>
 
-#include <algorithm>
-#include <cassert>
-#include <memory>
-#include <vector>
+#ifdef __CYGWIN__
+#include <cygwin/version.h>
+#if defined(CYGWIN_VERSION_DLL_MAJOR) && CYGWIN_VERSION_DLL_MAJOR < 1007
+#define DO_NOTHING_ATEXIT 1
+#endif
+#endif
 
 using namespace llvm;
 
-typedef std::unique_ptr<Module> LLVMModule;
-typedef std::unique_ptr<ExecutionEngine> LLVMEngine;
 
-LLVMContext _context;
-SMDiagnostic _error;
-
-
-void initialize() {
-    InitializeNativeTarget();
+LLVM_ATTRIBUTE_NORETURN
+static void reportError(SMDiagnostic Err, const char *ProgName) {
+  Err.print(ProgName, errs());
+  exit(1);
 }
 
+//===----------------------------------------------------------------------===//
+// main Driver function
+//
+int main(int argc, char **argv, char *const *envp) {
+  sys::PrintStackTraceOnErrorSignal(argv[0]);
+  PrettyStackTraceProgram X(argc, argv);
 
-void printIR(LLVMModule& module) {
-    if (module != NULL) {
-        outs() << *(module.get());    
-        outs().flush();
-    }
+  atexit(llvm_shutdown); // Call llvm_shutdown() on exit.
+
+  // if (argc > 1)
+  // ExitOnErr.setBanner(std::string(argv[0]) + ": ");
+
+  // If we have a native target, initialize it to ensure it is linked in and
+  // usable by the JIT.
+  InitializeNativeTarget();
+  InitializeNativeTargetAsmPrinter();
+  // InitializeNativeTargetAsmParser();
+
+  sys::Process::PreventCoreFiles();
+
+  LLVMContext Context;
+
+  // Load the bitcode...
+  SMDiagnostic Err;
+  std::string InputFile =
+      "/home/xuepeng/git/go-nebulas/nbre/test/link_example/foo.bc";
+  std::string sopath =
+      "/home/xuepeng/git/go-nebulas/nbre/test/link_example/libtest_bar.so";
+  llvm::sys::DynamicLibrary::LoadLibraryPermanently(sopath.c_str());
+  std::unique_ptr<Module> Owner = parseIRFile(InputFile, Err, Context);
+  Module *Mod = Owner.get();
+  if (!Mod)
+    reportError(Err, argv[0]);
+
+  std::vector<std::unique_ptr<Module>> Ms;
+  Ms.push_back(std::move(Owner));
+  // for (auto &ExtraMod : ExtraModules) {
+  // Ms.push_back(parseIRFile(ExtraMod, Err, Context));
+  // if (!Ms.back())
+  // reportError(Err, argv[0]);
+  //}
+  std::vector<std::string> Args;
+  Args.push_back(InputFile);
+  // for (auto &Arg : InputArgv)
+  // Args.push_back(Arg);
+  return runOrcLazyJIT(std::move(Ms), Args);
 }
-
-
-LLVMModule getModuleFromIRFile(char* fileName) {
-    return parseIRFile(StringRef(fileName), _error, _context);
-    //return getLazyIRFileModule(StringRef(fileName), _error, _context);
-}
-
-
-LLVMEngine createEngine(LLVMModule& module) {
-    std::string error;
-
-    ExecutionEngine* engine = EngineBuilder(std::move(module))
-        .setErrorStr(&error)
-        .create();
-
-    return std::unique_ptr<ExecutionEngine> (engine); 
-}
-
-
-GenericValue runFunction(ExecutionEngine* engine, Function* funcMain) {
-    GenericValue gv;
-    if (NULL != funcMain) {
-        std::vector<GenericValue> noargs;
-        gv = engine->runFunction(funcMain, noargs);
-    }
-
-    return gv;
-}
-
-
-void printResult(const GenericValue& gv) {
-    outs() << "Result: " << gv.IntVal << "\n";
-}
-
-
-void shutdownLLVM() {
-    llvm_shutdown();
-}
-
-
-void linkModule(Linker& linker, LLVMModule& module) {
-    bool error = linker.linkInModule(std::move(module), Linker::Flags::None & Linker::Flags::OverrideFromSrc);
-    assert(error != true);
-}
-
-
-LLVMModule linkFooAndBarModule(LLVMModule& fooModule, LLVMModule& barModule) {
-    LLVMModule compositeModule = make_unique<Module>("llvm-link", _context);
-    Linker linker(*compositeModule);
-
-    linkModule(linker, fooModule);
-    linkModule(linker, barModule);
-    
-    return compositeModule;
-}
-
-
-int main(int argc, char * argv[]) {
-
-    if (argc < 3) {
-        errs() << "Expected an argument - IR file name\n";
-        exit(1);
-    }
-
-    initialize();
-
-    LLVMModule fooModule = getModuleFromIRFile(argv[1]);
-    LLVMModule barModule = getModuleFromIRFile(argv[2]);
-
-    LLVMModule compositeModule = linkFooAndBarModule(fooModule, barModule);
-
-    printIR(compositeModule);
-
-    Function* funcFoo = compositeModule->getFunction("main");
-
-    LLVMEngine engine = createEngine(compositeModule);
-
-    GenericValue gv = runFunction(engine.get(), funcFoo);
-
-    printResult(gv);
-
-    shutdownLLVM();
-
-    return 0;
-}
-
-
