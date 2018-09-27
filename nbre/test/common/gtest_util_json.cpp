b0f499c0f83c11b3207a6d4981079248643fe26a
mrwangyu2 <wangyuxxx@163.com>
2018-09-27 15:26:06 +0800

    Fixed test case for nasir

--- nbre/test/common/gtest_util_json.cpp
+++ nbre/test/common/gtest_util_json.cpp
@@ -2,13 +2,13 @@
 #include <gtest/gtest.h>
 
 TEST(test_common_json_util, read_json) {
-  neb::ir_conf_reader json_reader("../test/data/test.json");
+  neb::ir_conf_reader json_reader("../test/data/test_nasir.json");
   EXPECT_EQ(json_reader.self_ref().name(), "xxx");
   EXPECT_EQ(json_reader.self_ref().version().major_version(), 1);
   EXPECT_EQ(json_reader.self_ref().version().minor_version(), 2);
   EXPECT_EQ(json_reader.self_ref().version().patch_version(), 3);
   EXPECT_EQ(json_reader.available_height(), 15);
-  EXPECT_EQ(json_reader.ir_fp(), "ir_file");
+  EXPECT_EQ(json_reader.ir_fp(), "../runtime/bar.bc");
   EXPECT_EQ(json_reader.depends()[0].name(), "yyy");
   EXPECT_EQ(json_reader.depends()[0].version().major_version(), 11);
   EXPECT_EQ(json_reader.depends()[0].version().minor_version(), 12);
@@ -23,21 +23,6 @@ TEST(test_common_json_util, throw_json) {
   EXPECT_THROW(neb::ir_conf_reader json_reader("xxx"),
                neb::json_general_failure);
 
-  neb::ir_conf_reader json_reader("../test/data/test_throw_exceptions.json");
-  EXPECT_THROW(json_reader.self_ref().name(), neb::json_general_failure);
-  EXPECT_THROW(json_reader.self_ref().version().major_version(),
-               neb::json_general_failure);
-  EXPECT_THROW(json_reader.self_ref().version().minor_version(),
-               neb::json_general_failure);
-  EXPECT_THROW(json_reader.self_ref().version().patch_version(),
-               neb::json_general_failure);
-  EXPECT_THROW(json_reader.available_height(), neb::json_general_failure);
-  EXPECT_THROW(json_reader.ir_fp(), neb::json_general_failure);
-  EXPECT_THROW(json_reader.depends()[0].name(), neb::json_general_failure);
-  EXPECT_THROW(json_reader.depends()[0].version().major_version(),
-               neb::json_general_failure);
-  EXPECT_THROW(json_reader.depends()[0].version().minor_version(),
-               neb::json_general_failure);
-  EXPECT_THROW(json_reader.depends()[0].version().patch_version(),
+  EXPECT_THROW(neb::ir_conf_reader json_reader("../test/data/test_throw_exceptions.json"),
                neb::json_general_failure);
 }
