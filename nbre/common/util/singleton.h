2f46f2df4d7602432f6f4264135626f32ecce2d3
Xuepeng Fan <athrunarthur@gmail.com>
2018-09-28 14:48:29 +0800

    add release for singleton

--- nbre/common/util/singleton.h
+++ nbre/common/util/singleton.h
@@ -28,20 +28,28 @@ namespace util {
 template <typename T> class singleton : boost::noncopyable {
 public:
   static T &instance() {
-    std::call_once(s_oOnce, std::bind(singleton<T>::init));
+    std::call_once(s_init_once, std::bind(singleton<T>::init));
     return *s_pInstance;
   }
+
+  void release() {
+    std::call_once(s_dealloc_once, std::bind(singleton<T>::dealloc));
+  }
+
 protected:
-  singleton() {}
+  singleton() = delete;
 
 private:
   static void init() { s_pInstance = std::shared_ptr<T>(new T()); }
+  static void dealloc() { s_pInstance.reset(nullptr); }
 
 protected:
   static std::shared_ptr<T> s_pInstance;
-  static std::once_flag s_oOnce;
+  static std::once_flag s_init_once;
+  static std::once_flag s_dealloc_once;
 };
 template <typename T> std::shared_ptr<T> singleton<T>::s_pInstance;
-template <typename T> std::once_flag singleton<T>::s_oOnce;
+template <typename T> std::once_flag singleton<T>::s_init_once;
+template <typename T> std::once_flag singleton<T>::s_dealloc_once;
 }
 }
