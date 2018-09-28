a3c30f36e978e6049fd161dd4b171bdfbcb1c3d9
mrwangyu2 <wangyuxxx@163.com>
2018-09-28 17:23:21 +0800

    Remove comments.

--- nbre/test/common/gtest_util_conf.cpp
+++ nbre/test/common/gtest_util_conf.cpp
@@ -5,11 +5,3 @@ TEST(test_common_configuration, read_config) {
   EXPECT_EQ(neb::configuration::instance().exec_name(), "bar");
   EXPECT_EQ(neb::configuration::instance().runtime_library_path(), "./lib");
 }
-//
-// TEST(test_common_json_util, throw_json) {
-  // EXPECT_THROW(neb::ir_conf_reader json_reader("xxx"),
-               // neb::json_general_failure);
-//
-  // EXPECT_THROW(neb::ir_conf_reader json_reader("../test/data/test_throw_exceptions.json"),
-               // neb::json_general_failure);
-//}
