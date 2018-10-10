8a69d785afb14297f53a175422407e98a6dbfa6f
chmwang <chmwang@zju.edu.cn>
2018-10-10 19:04:21 +0800

    remove gflags, instead of boost option

--- nbre/cmd/naxer/main.cpp
+++ nbre/cmd/naxer/main.cpp
@@ -18,19 +18,37 @@
 // <http://www.gnu.org/licenses/>.
 //
 
-#include "common/ir_conf_reader.h"
 #include "common/configuration.h"
+#include "common/ir_conf_reader.h"
 #include "core/ir_warden.h"
 #include "jit/jit_driver.h"
-#include <gflags/gflags.h>
+#include <boost/program_options.hpp>
 
-DEFINE_string(module, "nr", "module name");
-DEFINE_int64(height, 24000, "block height");
+namespace po = boost::program_options;
 
 int main(int argc, char *argv[]) {
-  google::ParseCommandLineFlags(&argc, &argv, true);
-  std::string module = FLAGS_module;
-  neb::block_height_t height = FLAGS_height;
+  po::options_description desc("Rocksdb read and write");
+  desc.add_options()("help", "show help message")(
+      "module", po::value<std::string>(), "Module name")(
+      "height", po::value<neb::block_height_t>(), "block height");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);
+
+  if (vm.count("help")) {
+    std::cout << desc << "\n";
+    return 1;
+  }
+
+  if (!vm.count("module")) {
+    std::cout << "You must specify \"module\"!" << std::endl;
+    return 1;
+  }
+  if (!vm.count("height")) {
+    std::cout << "You must specify \"height\"!" << std::endl;
+    return 1;
+  }
 
   // naxer --module nr --height 1000
 
@@ -38,6 +56,8 @@ int main(int argc, char *argv[]) {
 
   neb::core::ir_warden::instance().wait_until_sync();
 
+  std::string module = vm["module"].as<std::string>();
+  neb::block_height_t height = vm["height"].as<neb::block_height_t>();
   auto irs =
       neb::core::ir_warden::instance().get_ir_by_name_height(module, height);
 
