040b1510debbe254118e02b888fc24f08721b2bd
chmwang <chmwang@zju.edu.cn>
2018-10-10 19:42:49 +0800

    set read and write db directory

--- nbre/env.set.sh
+++ nbre/env.set.sh
@@ -6,5 +6,5 @@ export CC=$CUR_DIR/lib/bin/clang
 export PATH=$CUR_DIR/lib/bin:$PATH
 export LD_LIBRARY_PATH=$CUR_DIR/lib/lib:$LD_LIBRARY_PATH
 
-export NBRE_DB=$CUR_DIR/test_data.db
-export BLOCKCHAIN_DB=$CUR_DIR/test/data/data.db
+export NBRE_DB=$CUR_DIR/test/data/write-data.db
+export BLOCKCHAIN_DB=$CUR_DIR/test/data/read-data.db
