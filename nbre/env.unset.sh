fe1093da51d183801c9580b487632d1de3156481
Larry Wang <larry@nebulas.io>
2018-10-11 13:48:52 +0800

    nbre: update env set

--- nbre/env.unset.sh
+++ nbre/env.unset.sh
@@ -1,7 +1,11 @@
 
-CUR_DIR="$( pwd )"
-export AR
-export CXX
-export CC
+# CUR_DIR="$( pwd )"
+CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null && pwd  )"
+unset AR
+unset CXX
+unset CC
+unset NBRE_DB
+unset BLOCKCHAIN_DB
+
 export PATH
 export LD_LIBRARY_PATH
