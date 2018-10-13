3911adf388a471165faed1ba65214e02be2e1587
Xuepeng Fan <athrunarthur@gmail.com>
2018-10-13 16:18:25 +0800

    fix compilation error

--- nbre/jit/jit_exception.h
+++ nbre/jit/jit_exception.h
@@ -1,5 +1,6 @@
 // Copyright (C) 2018 go-nebulas authors
 //
+//
 // This file is part of the go-nebulas library.
 //
 // the go-nebulas library is free software: you can redistribute it and/or
@@ -17,31 +18,18 @@
 // along with the go-nebulas library.  If not, see
 // <http://www.gnu.org/licenses/>.
 //
-#include "core/neb_ipc/ipc_interface.h"
-#include "core/neb_ipc/ipc_callback_holder.h"
-#include "core/neb_ipc/ipc_endpoint.h"
+#pragma once
 
-std::shared_ptr<neb::core::ipc_endpoint> _ipc;
+#include "common/common.h"
 
-int start_nbre_ipc(const char *root_dir, const char *nbre_path) {
-  try {
-    _ipc = std::make_shared<neb::core::ipc_endpoint>(root_dir, nbre_path);
-    _ipc->start();
-    return 0;
-  } catch (const std::exception &e) {
-    return 1;
-  } catch (...) {
-    return 1;
-  }
+namespace neb {
+struct jit_internal_failure : public std::exception {
+public:
+  jit_internal_failure(const std::string &msg) : m_msg(msg) {}
+
+  inline const char *what() const throw() { return m_msg.c_str(); }
+
+protected:
+  std::string m_msg;
+};
 }
-
-void nbre_ipc_shutdown() {
-  _ipc->shutdown();
-  _ipc.reset();
-}
-
-void set_recv_nbre_version_callback(nbre_version_callback_t *func) {
-  neb::core::ipc_callback_holder::instance().m_nbre_version_callback = func;
-}
-
-void ipc_nbre_version() { _ipc->send_nbre_version_req(); }
